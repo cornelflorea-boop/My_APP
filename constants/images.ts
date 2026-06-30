d28cc721d6f0ed93cfb370bed8c0b67b3ae7b79e
cornel.florea <cornel.florea@rakuten.com>
2026-06-30 14:37:33 +0200

    add expo-localization plugin to app.json and track constants/images.ts

---
+++ constants/images.ts
@@ -0,0 +1,10 @@
+export const Images = {
+  treasure: require("../assets/images/treasure.png"),
+  palace: require("../assets/images/palace.png"),
+  streakFire: require("../assets/images/streak-fire.png"),
+  mascotWelcome: require("../assets/images/mascot-welcome.png"),
+  mascotAuth: require("../assets/images/mascot-auth.png"),
+  moscotLogo: require("../assets/images/moscot-logo.png"),
+  earth: require("../assets/images/earth.png"),
+  icon: require("../assets/images/icon.png"),
+};
